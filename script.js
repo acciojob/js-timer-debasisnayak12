f186b844139ccf6ec586d24ff80234007c2fa552
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-12-06 17:22:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // write js code here if required
+const timer = document.getElementById("timer");
+
+setInterval(()=>{
+	const instance = new Date();
+	const date = `${instance.getDate()}/${instance.getMonth()}/${instance.getFullYear()}`;
+	const time = `${instance.getHours()}:${instance.getMinutes()}:${instance.getSeconds()}`;
+	timer.textContent = `${date}, ${time}`;
+});
